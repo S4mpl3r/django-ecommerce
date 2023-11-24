85db9c14b36dd7f660bd008d60a4d3ce87b575bd
moji99 <test@test.com>
2023-11-25 01:45:34 +0330

    Added the search and browse functionality.

--- items/urls.py
+++ items/urls.py
@@ -7,4 +7,5 @@ app_name = "items"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:pk>/", views.detail, name="detail"),
+    path("browse/", views.browse, name="browse"),
 ]
--- items/views.py
+++ items/views.py
@@ -1,6 +1,7 @@
+from django.db.models import Q
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
-from .models import Item
+from .models import Category, Item
 
 
 # Create your views here.
@@ -27,5 +28,31 @@ def detail(request, pk):
         "items/detail.html",
         {
             "item": item,
+        },
+    )
+
+
+def browse(request):
+    query = request.GET.get("query", "")
+    category_id = request.GET.get("category", 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(
+            Q(title__icontains=query) | Q(description__icontains=query)
+        )
+
+    return render(
+        request,
+        "items/browse.html",
+        {
+            "items": items,
+            "query": query,
+            "categories": categories,
+            "category_id": int(category_id),
         },
     )
